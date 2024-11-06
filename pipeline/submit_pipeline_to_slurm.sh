db0adf103218b16d85b749c1b90536c378053b19
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2024-11-06 17:13:09 -0500

    setting env path as input parameter

--- pipeline/submit_pipeline_to_slurm.sh
+++ pipeline/submit_pipeline_to_slurm.sh
@@ -5,16 +5,23 @@
 #SBATCH --partition={your-partition}
 #SBATCH --time=2:00:00
 
+# Check if the path is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 /path/to/environment"
+  exit 1
+fi
+
 # modify this section to make the nextflow command available to your environment
 # e.g., using a conda environment with nextflow installed
-conda activate env_nf
+ENV_PATH="$1"
+conda activate "$ENV_PATH"
 
 PIPELINE_PATH="path-to-your-cloned-repo"
 DATA_PATH="path-to-data-folder"
 RESULTS_PATH="path-to-results-folder"
 WORKDIR="path-to-workdir-folder"
 
-NXF_VER=22.10.8 DATA_PATH=$DATA_PATH RESULTS_PATH=$RESULTS_PATH nextflow \
+NXF_VER=24.10.0.5928 DATA_PATH=$DATA_PATH RESULTS_PATH=$RESULTS_PATH nextflow \
     -C $PIPELINE_PATH/pipeline/nextflow_slurm.config \
     -log $RESULTS_PATH/nextflow/nextflow.log \
     run $PIPELINE_PATH/pipeline/main_slurm.nf \
