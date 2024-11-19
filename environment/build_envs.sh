7c655186a4b10c2513418f51baa98d71935e917f
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2024-11-19 16:55:41 -0500

    updating bash script to download all the dockerfiles, build them, tag them before publishing

--- environment/build_envs.sh
+++ environment/build_envs.sh
@@ -1,3 +1,14 @@
+# Download the Dockerfiles
+wget -O Dockerfile_validation https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-validation/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_preprocessing https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-destripe/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_stitch https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-stitch/refs/heads/feat-only-stitching/environment/Dockerfile_local
+wget -O Dockerfile_registration https://raw.githubusercontent.com/AllenNeuralDynamics/aind-ccf-registration/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_fuse https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-fuse/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_dispatcher https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-external-dispatcher/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_cell_detection https://raw.githubusercontent.com/AllenNeuralDynamics/aind-SmartSPIM-segmentation/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_cell_classification https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-classification/refs/heads/main/environment/Dockerfile_local
+wget -O Dockerfile_cell_quantification https://raw.githubusercontent.com/AllenNeuralDynamics/aind-smartspim-quantification/refs/heads/main/environment/Dockerfile_local
+
 docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-validation:si-0.0.1 -f Dockerfile_validation .
 docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-preprocessing:si-0.0.3 -f Dockerfile_preprocessing .
 docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-stitch:si-1.2.4 -f Dockerfile_stitch .
@@ -7,3 +18,5 @@ docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-dispatch:si-0.0.1 -f 
 docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-cell-detection:si-0.0.6 -f Dockerfile_cell_detection .
 docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-cell-classification:si-0.0.2 -f Dockerfile_cell_classification .
 docker build -t ghcr.io/allenneuraldynamics/aind-smartspim-cell-quantification:si-1.0.1 -f Dockerfile_cell_quantification .
+
+rm Dockerfile_*
