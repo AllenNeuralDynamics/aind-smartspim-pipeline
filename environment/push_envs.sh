6a092aaa6ad32231fb58b8a71368e7bfda9e7df6
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2026-05-14 14:15:42 -0700

    parametrizing push environments

--- environment/push_envs.sh
+++ environment/push_envs.sh
@@ -3,22 +3,28 @@
 # before running this script
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-flatfield-estimation:si-0.0.1 ghcr.io/allenneuraldynamics/aind-smartspim-flatfield-estimation:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-preprocessing:si-0.0.4 ghcr.io/allenneuraldynamics/aind-smartspim-preprocessing:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-stitch:si-1.2.7 ghcr.io/allenneuraldynamics/aind-smartspim-stitch:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-registration:si-0.0.31 ghcr.io/allenneuraldynamics/aind-smartspim-registration:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-fuse:si-0.0.4 ghcr.io/allenneuraldynamics/aind-smartspim-fuse:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-dispatch:si-1.0.1 ghcr.io/allenneuraldynamics/aind-smartspim-dispatch:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-cell-detection:si-1.0.0 ghcr.io/allenneuraldynamics/aind-smartspim-cell-detection:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-cell-classification:si-0.0.6 ghcr.io/allenneuraldynamics/aind-smartspim-cell-classification:latest
-docker tag ghcr.io/allenneuraldynamics/aind-smartspim-cell-quantification:si-1.6.1 ghcr.io/allenneuraldynamics/aind-smartspim-cell-quantification:latest
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck source=versions.env
+source "${SCRIPT_DIR}/versions.env"
 
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-flatfield-estimation
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-preprocessing
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-stitch
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-registration
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-fuse
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-dispatch
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-cell-detection
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-cell-classification
-docker push --all-tags ghcr.io/allenneuraldynamics/aind-smartspim-cell-quantification
+ORG="ghcr.io/allenneuraldynamics"
+
+docker tag "${ORG}/aind-smartspim-flatfield-estimation:${FLATFIELD_EST_VERSION}" "${ORG}/aind-smartspim-flatfield-estimation:latest"
+docker tag "${ORG}/aind-smartspim-preprocessing:${PREPROCESSING_VERSION}" "${ORG}/aind-smartspim-preprocessing:latest"
+docker tag "${ORG}/aind-smartspim-stitch:${STITCH_VERSION}" "${ORG}/aind-smartspim-stitch:latest"
+docker tag "${ORG}/aind-smartspim-registration:${REGISTRATION_VERSION}" "${ORG}/aind-smartspim-registration:latest"
+docker tag "${ORG}/aind-smartspim-fuse:${FUSE_VERSION}" "${ORG}/aind-smartspim-fuse:latest"
+docker tag "${ORG}/aind-smartspim-dispatch:${DISPATCH_VERSION}" "${ORG}/aind-smartspim-dispatch:latest"
+docker tag "${ORG}/aind-smartspim-cell-detection:${CELL_DETECTION_VERSION}" "${ORG}/aind-smartspim-cell-detection:latest"
+docker tag "${ORG}/aind-smartspim-cell-classification:${CELL_CLASSIFICATION_VERSION}" "${ORG}/aind-smartspim-cell-classification:latest"
+docker tag "${ORG}/aind-smartspim-cell-quantification:${CELL_QUANTIFICATION_VERSION}" "${ORG}/aind-smartspim-cell-quantification:latest"
+
+docker push --all-tags "${ORG}/aind-smartspim-flatfield-estimation"
+docker push --all-tags "${ORG}/aind-smartspim-preprocessing"
+docker push --all-tags "${ORG}/aind-smartspim-stitch"
+docker push --all-tags "${ORG}/aind-smartspim-registration"
+docker push --all-tags "${ORG}/aind-smartspim-fuse"
+docker push --all-tags "${ORG}/aind-smartspim-dispatch"
+docker push --all-tags "${ORG}/aind-smartspim-cell-detection"
+docker push --all-tags "${ORG}/aind-smartspim-cell-classification"
+docker push --all-tags "${ORG}/aind-smartspim-cell-quantification"
